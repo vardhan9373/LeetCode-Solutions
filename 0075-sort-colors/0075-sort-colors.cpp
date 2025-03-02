4a6cbb801c6cc8d4bb49b4d54d630f555fc5e58c
vardhan9373 <146452086+vardhan9373@users.noreply.github.com>
2025-03-02 13:23:03 +0530

    Time: 0 ms (100%), Space: 11.7 MB (5.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        multiset<int>m;
-        int v=0;
-        for(int i=0;i<nums.size();i++){
-            m.insert(nums[i]);
-        }
-        for(auto it:m)
+    void sortColors(vector<int>& arr) {
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<arr.size();i++)
         {
-            nums[v]=it;
-            v++;
+            if(arr[i]==0)
+            {
+               count0++;
+            }
+            else if(arr[i]==1)
+            {
+                count1++;
+            }
+            else if(arr[i]==2)
+            {
+                count2++;
+            }
         }
-
+        int index=0;
+        for(int i=0;i<count0;i++)
+        {
+            arr[index++]=0;
+        }
+         for(int i=0;i<count1;i++)
+        {
+            arr[index++]=1;
+        }
+         for(int i=0;i<count2;i++)
+        {
+            arr[index++]=2;
+        }
     }
+    
 };
